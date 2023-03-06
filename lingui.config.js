dfdd08c826cd3835f8d459a99581ebe0adbde5f4
Timofei Iatsenko <ty@time.sc>
2023-03-06 16:27:48 +0100

    update repo for current lingui state

--- lingui.config.js
+++ lingui.config.js
@@ -1,14 +1,25 @@
+const nextConfig = require('./next.config');
+
+/** @type {import('@lingui/conf').LinguiConfig} */
 module.exports = {
-  locales: ['en', 'sr', 'es', 'pseudo'],
+  locales: nextConfig.i18n.locales,
   pseudoLocale: 'pseudo',
-  sourceLocale: 'en',
+  sourceLocale: nextConfig.i18n.defaultLocale,
+  // this is crucial to make `lingui extract` work in nextjs with swc compiler in v3
+  // and can be safely removed in v4 (upcoming release)
+  extractBabelOptions: {
+    presets: [
+      "@babel/preset-typescript",
+      "@babel/preset-react",
+    ],
+  },
   fallbackLocales: {
     default: 'en'
   },
   catalogs: [
     {
-      path: 'src/translations/locales/{locale}/messages',
-      include: ['src/pages', 'src/components']
+      path: 'src/locales/{locale}',
+      include: ['src/']
     }
   ],
   format: 'po'
