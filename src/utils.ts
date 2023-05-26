da426c64b9106c039d5e51c0269db7009f69401f
Timofei Iatsenko <ty@road.travel>
2023-05-26 13:19:48 +0200

    migrate demo for lingui v4

--- src/utils.ts
+++ src/utils.ts
@@ -12,16 +12,6 @@ export function activateLocale(i18n: I18n, locale: string, messages: Messages) {
     locale = "en";
   }
 
-  const cardinalRules = new Intl.PluralRules(locale, { type: 'cardinal' });
-  const ordinalRules = new Intl.PluralRules(locale, { type: 'ordinal' });
-
-  i18n.loadLocaleData(locale, {
-    plurals: (n, ordinal) => {
-      return (ordinal ? ordinalRules : cardinalRules).select(n);
-    },
-  });
-
-  i18n.load(locale, messages)
-  i18n.activate(locale)
-
+  i18n.loadAndActivate({ locale, messages })
 }
+
